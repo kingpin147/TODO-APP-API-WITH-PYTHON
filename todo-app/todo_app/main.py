6da1579c72ebf76063ef5f7237c10f03385238b7
kingpin147 <nomiking0072012@gmail.com>
2024-05-08 14:42:42 +0500

    docker file added

--- todo-app/todo_app/main.py
+++ todo-app/todo_app/main.py
@@ -30,7 +30,7 @@ class TODO (SQLModel, table=True):
 
 # creating database connection
 connection_string : str = str(setting.DATABASE_URL).replace("postgresql", "postgresql+psycopg")
-engine = create_engine(connection_string, connect_args={"sslmode":"require"}, pool_recycle=300, pool_size=10, echo=True)
+engine = create_engine(connection_string, connect_args={"sslmode":"require"}, pool_recycle=300, pool_size=10, echo=False)
 
 def create_tables():
     SQLModel.metadata.create_all(engine)
